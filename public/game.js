ec0b49bef649722fa26cd18770437736b2081ef4
Thang Le <leminhthang.tnl@gmail.com>
2020-10-22 23:19:07 -0500

    Update Hit method

--- public/game.js
+++ public/game.js
@@ -6,30 +6,36 @@ let betAmount = 0;
 let currentPhase = 'player phase'
 
 
-let cardArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'];//Array for suits
-let suitArray = ['diamond', 'heart', 'spade', 'club']
+let cardArray = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];//Array for suits
+let suitArray = ['D', 'H', 'S', 'C']
 let playerHand = []//Arbitrary number
 let dealerHand = []//Arbitrary number
-
+let mode = ""
+let singlePlayerButton = document.getElementById('btn-single')
 let hitButton = document.getElementById('hitButton')
-document.addEventListener('DOMContentLoaded', function () {
-    console.log(document)
-});
+
+// singlePlayerButton.addEventListener("click", () => {
+//     mode = "singlePlayer"
+//     console.log(mode)
+// })
+
 hitButton.addEventListener("click", Hit)
 
 function Hit(){
+    // console.log(mode)
     if(currentPhase === 'player phase'){
         let cardDrawn = ''
         do{
             console.log("Hit get called")
             let randomCard = cardArray[Math.floor(Math.random()*cardArray.length)]
             let randomSuit = suitArray[Math.floor(Math.random()*suitArray.length)]
-            cardDrawn = randomCard + ' '+ randomSuit;
+            cardDrawn = randomCard + randomSuit;
         }while(cardDrawn in playerHand)
 
         playerHand.push(cardDrawn)
-        
-        document.querySelector('#player-hand').innerHTML += "<div class='card-player'><p>"+cardDrawn+"</p></div>"
+        let imgName = cardDrawn+'.png'
+        console.log(imgName)
+        document.querySelector('#player-hand').innerHTML += "<div class='card-player'><img class = 'card-img' src = 'image/cards/"+imgName+"'></div>"
     }
 }
 
@@ -40,11 +46,11 @@ function dealerDraw(){
             console.log("Hit get called")
             let randomCard = cardArray[Math.floor(Math.random()*cardArray.length)]
             let randomSuit = suitArray[Math.floor(Math.random()*suitArray.length)]
-            cardDrawn = randomCard + ' '+ randomSuit;
+            cardDrawn = randomCard + randomSuit;
         }while(cardDrawn in playerHand)
-
+        let imgName = cardDrawn+'.png'
         playerHand.push(cardDrawn)
-        document.querySelector('#player-hand').innerHTML += "<div class='card-player'><p>"+cardDrawn+"</p></div>"
+        document.querySelector('#player-hand').innerHTML += "<div class='card-player'><img src = '"+imgName+"'></div>"
     }
 }
 
