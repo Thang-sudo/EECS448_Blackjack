4853551ed7d3c5b922ac5af0d35248afd4989704
gkhenry142 <47468627+gkhenry142@users.noreply.github.com>
2020-10-22 22:19:58 -0500

    Gave names to cards and buttons

--- public/game.js
+++ public/game.js
@@ -5,25 +5,40 @@ let currentBalance = 200;
 let betAmount = 0;
 
 
-let cardArray = new Array(13);//Array for suits
-let suitArray = new Array(4);
-let playerDeck = new Array(5);//Arbitrary number
-let dealerDeck = new Array (5);//Arbitrary number
+let cardArray =['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']//Array for suits
+let suitArray = ["diamonds", "hearts", "spades", "clubs"];
+let playerHand = []//Arbitrary number
+let dealerHand = []//Arbitrary number
+
+const m_player1Card1 = document.getElementById("player1Card1");
+const m_player1Card2 = document.getElementById("player1Card2");
+const m_player1Card3 = document.getElementById("player1Card3");
+const m_player1Card4 = document.getElementById("player1Card4");
+const m_player1Card5 = document.getElementById("player1Card5");
+const m_player1Card6 = document.getElementById("player1Card6");
+const m_playerBalance = document.getElementById("balance");
+
+const m_dealerCard1 = document.getElementById("dealerCard1");
+const m_dealerCard2 = document.getElementById("dealerCard2");
 
 
+const m_hitButton = document.getElementById("hitButton");
+const m_stayButton = document.getElementById("stayButton");
 
-for(let i = 0; i < 4; i++)
-{
-    suitArray[i] = new Array(13);
+document.addEventListener('DOMContentLoaded', function() {
+    console.log(document);
+});
+m_hitButton.addEventListener("click", Hit);
+
+function Hit(){
+    console.log("Hit Called");
+    let randomCard = cardArray[Math.floor(Math.random()*cardArray.length)]
+    let randomSuit = suitArray[Math.floor(Math.random()*suitArray.length)]
+    console.log(randomCard);
+    console.log(randomSuit);
+    let cardDrawn = randomCard + randomSuit;
 }
 
-for(let i = 0; i < 4; i++)
-{
-    for(let j = 0; j < 13; j++)
-    {
-        suitArray[i][j] = "-";
-    }
-}
 /*Methods:
 
 
