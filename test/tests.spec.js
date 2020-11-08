3530f69d7e0f83f89558af4420753c42bcaed158
Wenxuan Hu <wenxuanhu66@gmail.com>
2020-11-08 00:22:03 -0600

    test suite could work now

--- test/tests.spec.js
+++ test/tests.spec.js
@@ -1,1 +1,18 @@
-var expect = chai.expect;
+//for test
+
+var should = chai.should();
+
+describe("Start Multiplayer Mode Test",function(){
+    it("If number not equal -1",function(){
+        var number=startMultiPlayerMode();
+        should.equal(number,playerNum)
+    });
+});
+
+describe("Hit function Test",function(){
+    it("if Hit",function(){
+        var number=Hit();
+        should.equal(number,playerNum)
+    });
+});
+
